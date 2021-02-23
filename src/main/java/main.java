e0ae5a718fcc90596cbf6cb55659f185a05533c7
IfrimAndrei <andrei_ifrim@yahoo.com>
2021-02-23 11:18:10 +0200

    Version 1.3

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,26 +1,9 @@
 public class main {
-    public static void main ( String[] args){
+    public static void main(String[] args) {
 
         Compulsory.main();
         Optional.main(args);
         Bonus.main();
 
     }
-/*
-    private static void bonus() {
-        //Bonus obj3=new Bonus();
-        Bonus.main();
-    }
-
-    private static void optional(String[] args) {
-        Optional obj2=new Optional();
-        Optional.main(args);
-    }
-
-    private static void compulsory() {
-        Compulsory obj1=new Compulsory();
-        Compulsory.main();
-    }*/
-
-
 }
